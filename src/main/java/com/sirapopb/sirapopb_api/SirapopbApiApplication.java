82ce16e8e1375cd257c3a90f149cf563b3d0659b
Calvin Liang <145249380+LiangCalvin@users.noreply.github.com>
2025-03-22 18:15:53 +0700

    first commit

---
+++ src/main/java/com/sirapopb/sirapopb_api/SirapopbApiApplication.java
@@ -0,0 +1,13 @@
+package com.sirapopb.sirapopb_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirapopbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirapopbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sirapopb/sirapopb_api/SirapopbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sirapopb.sirapopb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirapopbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
